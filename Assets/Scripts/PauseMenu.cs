fe11862927d50a291037eb086f763e1751d71e23
Ilya <ilyavodop1@gmail.com>
2022-01-26 02:06:40 +0500

    Edits and create material, Utilites.cs

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance;
+
+    private CanvasGroup _canvasGroup;
+    private Coroutine _fadeAnimation;
+
+
+    public void SetPauseState(bool value)
+    {
+        gameObject.SetActive(value);
+
+        if (value)
+        {
+            _fadeAnimation = StartCoroutine(GameManager.Instance.AlphaCoroutine(0, 1f, _canvasGroup));
+        }
+        else
+        {
+            _canvasGroup.alpha = 0;
+        }
+    }
+    private void Start()
+    {
+        SetPauseState(false);
+    }
+    private void OnDisable()
+    {
+        if (_fadeAnimation != null) StopCoroutine(_fadeAnimation);
+    }
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+
+        Instance = null;
+    }
+}
